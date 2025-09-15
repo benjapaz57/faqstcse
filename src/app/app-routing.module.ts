bf10289f0d5e950c5f4b7d80d8ce8b614908470a
benja <benjapaz57@gmail.com>
2025-09-15 10:09:45 -0300

    organismos provinciales

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,10 +16,6 @@ import { OrdendepagoComponent } from './components/sarc/rendicionesdecuenta/orde
 import { SigepComponent } from './components/orgprovinciales/sigep/sigep.component';
 import { ConsultaComponent } from './components/orgprovinciales/consulta/consulta.component';
 import { AnticipoComponent } from './components/orgprovinciales/anticipo/anticipo.component';
-import { AdjsimpleComponent } from './components/orgprovinciales/sigep/adjsimple/adjsimple.component';
-import { BienesyservComponent } from './components/orgprovinciales/sigep/bienesyserv/bienesyserv.component';
-import { CompulsaComponent } from './components/orgprovinciales/sigep/compulsa/compulsa.component';
-import { ObraspublicasComponent } from './components/orgprovinciales/sigep/obraspublicas/obraspublicas.component';
 
 
 
@@ -45,11 +41,6 @@ const routes: Routes = [
   { path: 'sigep', component: SigepComponent},
   { path: 'consulta', component: ConsultaComponent},
   { path: 'anticipo', component: AnticipoComponent},
-  //SIGEP
-  { path: 'adjsimple', component: AdjsimpleComponent},
-  { path: 'bienesyserv', component: BienesyservComponent},
-  { path: 'compulsa', component: CompulsaComponent},
-  { path: 'obraspublicas', component: ObraspublicasComponent},
   //PageNotFound Component
   { path: '**', component: PagenotfoundComponent}
 
